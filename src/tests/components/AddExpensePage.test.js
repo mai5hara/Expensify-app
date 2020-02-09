72485f88525a86c5a73f65412e98636bcfe5116b
maigohara <maigohara-1103@outlook.jp>
2020-02-08 21:11:38 -0800

    Remove dammy expense data

---
+++ src/tests/components/AddExpensePage.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { AddExpensePage } from '../../components/AddExpensePage';
+import expenses from '../fictures/expenses';
+
+// test('should render AddExpensePage correctly', () => {
+//     const onSubmit = jest.fn();
+//     const history = { push: jest.fn() };
+//     const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history}/>);
+//     expect(wrapper).toMatchSnapshot();
+// });
+
+let addExpense, history, wrapper;	
+
+beforeEach(() => {
+    addExpense = jest.fn();
+    history = { push: jest.fn() };
+    wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />);
+});
+
+test('should render AddExpensePage correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle onSubmit', () => {
+    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
+    expect(history.push).toHaveBeenLastCalledWith('/');
+    expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
+});
