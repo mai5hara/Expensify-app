a7eadbc8a5c95b5c4c1a9ef992934bf10c2b869e
maigohara <maigohara-1103@outlook.jp>
2020-02-10 23:44:32 -0800

    Setup test database env

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,6 +1,6 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+// import Enzyme from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
 import DotEnv from 'dotenv';
 DotEnv.config({ path: '.env.test' });
 
-Enzyme.configure({ adapter: new Adapter() });
+// Enzyme.configure({ adapter: new Adapter() });
